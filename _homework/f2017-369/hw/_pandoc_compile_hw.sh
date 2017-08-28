040d94109000cfa65a8e97149ecbfadf344b3480
Harrison Chapman <hchaps@gmail.com>
2017-08-28 00:20:05 -0600

    add homework 2

--- _homework/f2017-369/hw/_pandoc_compile_hw.sh
+++ _homework/f2017-369/hw/_pandoc_compile_hw.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-pandoc -o $1.pdf -f markdown-fancy_lists -V fontsize=12pt -V geometry=margin=1in --template=../../_pandoc.template.latex $1.md
+# fml with this cross-compiling hack
+
+sed -e 's/^\(\s*\)\$\$\(\s*\)$/\1\%displaymath%\2/g' -e 's/\$\$/\$/g' -e 's/%displaymath%/$$/g' $1.md | \
+    pandoc -o $1.pdf -f markdown-fancy_lists -V fontsize=12pt -V geometry=margin=1in --template=../../_pandoc.template.latex
